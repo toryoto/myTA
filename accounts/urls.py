779c747f47e64c0d031a00223f05122f5adc24a1
toryoto <ryoto.taga12@gmail.com>
2025-06-08 02:02:00 +0900

    auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import CreateView
+
+app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', CreateView.as_view(
         template_name='accounts/signup.html',
         form_class=UserCreationForm,
-        success_url='/',
+        success_url=reverse_lazy('diary:index'),
     ), name='signup'),
     path('login/', LoginView.as_view(
         redirect_authenticated_user=True,
